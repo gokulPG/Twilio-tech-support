72c52a0038227975c7e54fdcc41304f8fede4683
Gokul P Gangadharan <gokulgangadharanp@gmail.com>
2021-01-27 17:34:49 +0530

    send and verify code sent implemented

--- backend/Twilio.js
+++ backend/Twilio.js
@@ -1,6 +1,44 @@
-const phoneNumber = process.env.phoneNumber;
-const phoneNumberSid = process.env.phoneNumberSid;
-const tokenSid = process.env.tokenSid;
-const tokenSecret = process.env.tokenSecret; 
-const accountSid = process.env.accountSid;
-const verify = process.env.verify;
+const twilio = require("twilio");
+
+class Twilio {
+  phoneNumber = process.env.PHONE_NUMBER;
+  phoneNumberSid = process.env.PHONE_NUMBER_SID;
+  tokenSid = process.env.TOKEN_SID;
+  tokenSecret = process.env.TOKEN_SECRET;
+  accountSid = process.env.ACCOUNT_SID;
+  verify = process.env.VERIFY;
+  client;
+  constructor() {
+    this.client = twilio(this.tokenSid, this.tokenSecret, {
+      accountSid: this.accountSid,
+    });
+  }
+
+  getTwilio() {
+    this.client;
+  }
+
+  async sendVerifyAsync(to, channel) {
+    const data = await this.client.verify
+      .services(this.verify)
+      .verifications.create({ to, channel });
+    return data;
+  }
+
+  async verifyCodeAsync(to, code) {
+    const data = await this.client.verify
+      .services(this.verify)
+      .verificationChecks.create({
+        to,
+        code,
+      });
+    console.log(data);
+    return;
+  }
+}
+
+const instance = new Twilio();
+
+Object.freeze(instance); //Cannot the change the instance, once its created
+
+module.exports = instance;
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const twilio = require('./Twilio')
+
+
 const app = express();
 const PORT = 3001;
-
 
 dotenv.config();
 
@@ -10,12 +12,16 @@ app.get('/test', (req, res) => {
     res.send('Welcome to twilio');
 })
 
-app.get('/login', (req,res) => {
+app.get('/login', async (req,res) => {
     console.log('logging in')
+    const data = await twilio.sendVerifyAsync('+91 93539 61459', 'sms')
+    res.send(data)
 })
 
-app.get('/verify', (req,res) => {
+app.get('/verify', async (req,res) => {
     console.log('Verifying code')
+    const data = await twilio.verifyCodeAsync('+91 93539 61459', req.query.code)
+    return data
 })
 
 
